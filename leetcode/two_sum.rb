90d414aff40b9843929b4f1b44c56e319f685118
River2056 <chen0625tung@gmail.com>
2022-05-20 13:22:59 +0800

    + initialize repository + added leetcode two_sum

---
+++ leetcode/two_sum.rb
@@ -0,0 +1,16 @@
+require "test/unit"
+
+def two_sum(nums, target)
+  ref = {}
+  for i in 0..nums.length do
+    if ref.key?(nums[i])
+      return [i, ref[nums[i]]]
+    else
+      diff = target - nums[i]
+      ref[diff] = i
+    end
+  end
+end
+
+result = two_sum([2, 7, 11, 15], 9)
+puts result
